d7b3ef045267f173944ecad695e57355792344dd
Nicolas Shanks <nicolasshanks10@gmail.com>
2022-09-01 20:29:42 -0400

    first commit

---
+++ homework.sql
@@ -0,0 +1,81 @@
+-- 1. How many actors are there with the last name ‘Wahlberg’?
+SELECT last_name
+FROM actor
+WHERE last_name = 'Wahlberg';
+-- 2
+
+-- 2. How many payments were made between $3.99 and $5.99?
+SELECT customer_id, amount
+FROM payment
+WHERE amount BETWEEN 3.99 AND 5.99;
+-- 5607
+
+-- 3. What film does the store have the most of? (search in inventory)
+SELECT film_id, COUNT(film_id)
+FROM inventory
+GROUP BY film_id
+ORDER BY COUNT(film_id) DESC;
+-- Bunch of films at 8
+
+-- 4. How many customers have the last name ‘William’?
+SELECT last_name
+FROM customer
+WHERE last_name = 'William';
+--0
+
+-- 5. What store employee (get the id) sold the most rentals?
+SELECT staff_id, COUNT(staff_id)
+FROM payment
+GROUP BY staff_id;
+-- 2, 7304?
+
+-- 6. How many different district names are there?
+SELECT district, COUNT(district)
+FROM address
+GROUP BY district;
+-- NOT DONE
+
+
+-- 7. What film has the most actors in it? (use film_actor table and get film_id)
+SELECT film_id
+FROM film_actor
+GROUP BY film_id
+ORDER BY COUNT(film_actor) DESC;
+-- 508
+
+-- 8. From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
+SELECT COUNT(last_name)
+FROM customer
+WHERE last_name LIKE '%es' AND store_id < 2
+-- 13
+
+
+-- 9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers
+-- with ids between 380 and 430? (use group by and having > 250)
+SELECT customer_id, amount
+FROM payment
+GROUP BY customer_id
+ORDER BY COUNT(amount) DESC;
+-- 
+
+
+
+-- 10. Within the film table, how many rating categories are there? And what rating has the most
+-- movies total?
+select rating, count(rating)
+from film
+group by rating;
+-- 5, PG-13
+
+
+
+
+
+
+
+
+
+
+
+
+
